29712671f6361248e226b1e2e893da4d08460d7b
grinevich <alexandr.grinevich@gmail.com>
2020-07-04 19:15:39 +0300

    slide 4. marquee text

--- src/components/slide-4/index.js
+++ src/components/slide-4/index.js
@@ -2,8 +2,71 @@ import React, { useEffect, useRef } from 'react'
 import style from './style.module.scss'
 
 export const Slide4 = () => {
+  const refText = useRef(null)
+  const refTextVert = useRef(null)
+
+  useEffect(() => {
+    window.gsap.to(refText.current.querySelectorAll('i'), 10, {
+      xPercent: '-=100',
+      ease: 'none',
+      repeat: -1
+    })
+
+    window.gsap.to(refTextVert.current.querySelectorAll('i'), 10, {
+      yPercent: '+=100',
+      ease: 'none',
+      repeat: -1
+    })
+    
+  }, [refText])
+
   return (
     <div className={style.slideRoot}>
+      <div ref={refText} className={style.rollingText}>
+        <i className={style.stripe}>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+        </i>
+        <i className={style.stripe}>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+        </i>
+      </div>
+
+      <div ref={refTextVert} className={style.rollingTextVert}>
+        <i className={style.stripe}>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+        </i>
+        <i className={style.stripe}>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+          <span className={style.text}>Wardrobe</span>
+        </i>
+      </div>
     </div>
   )
 }
