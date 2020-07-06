5d833d5280ac0913d3c7be02a52d3f7748f6b09d
grinevich <alexandr.grinevich@gmail.com>
2020-07-06 23:14:24 +0300

    slide 5. image gallery

--- src/components/slide-5/index.js
+++ src/components/slide-5/index.js
@@ -1,9 +1,33 @@
 import React, { useEffect, useRef } from 'react'
+import times from 'lodash/times'
 import style from './style.module.scss'
 
 export const Slide5 = () => {
+  const refRoot = useRef(null)
+
+  useEffect(() => {
+    const images = Array.from(refRoot.current.querySelectorAll('span'))
+    images.map((el, i) => {
+      const { width: imageWidth } = el.getBoundingClientRect()
+      const margin = imageWidth / 6
+      const totalWidth = images.length * imageWidth - images.length * margin
+
+      return window.gsap.fromTo(el, {
+        x: totalWidth - imageWidth
+      }, {
+        x: -imageWidth,
+        ease: 'none',
+        duration: 4 * images.length,
+        repeat: -1
+      }).progress(1 - (i * imageWidth - i * margin) / totalWidth)
+    })
+  }, [refRoot])
+
   return (
-    <div className={style.slideRoot}>
+    <div ref={refRoot} className={style.slideRoot}>
+      {times(8, (i) => (
+        <span className={style[`image-${i + 1}`]} key={`image.${i}`} />
+      ))}
     </div>
   )
 }
